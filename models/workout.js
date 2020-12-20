9937f76e8b5f06872e59698af1102577dcd04632
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-20 10:17:31 -0500

    started the skeleton set up of the workout schema in models

--- models/workout.js
+++ models/workout.js
@@ -3,7 +3,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-
+    day: {
+        type: Date,
+        default: Date.now()
+    },
+    exercises: [
+        {
+            type: "",
+            name: "",
+            duration: "",
+            weight: "",
+            reps: "",
+            sets: ""
+        }
+    ]
 })
 
 const Workout = mongoose.model("Workout", workoutSchema);
