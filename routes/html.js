5e65c5898fa55c5117d141dcf460736eb1b3c5d0
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-29 16:53:32 -0500

    added comments to html routes

--- routes/html.js
+++ routes/html.js
@@ -1,14 +1,17 @@
 var router = require("express").Router();
 var path = require("path");
 
+// route to the home page //
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+// route to the exercise page //
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
+//route to the stats page //
 router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
