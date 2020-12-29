fdd5a5fb0f25c71a848c8abceb7b00364f01c5ac
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-29 16:58:44 -0500

    psuedo coded api routes

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,12 @@
 const router = require("express").Router();
 const Workout= require("../models/workout.js");
 
+// get workout data //
+
+// create a new workout session //
+
+// update existing workout //
+
+// get stats for all workouts chart //
+
 module.exports = router;
