1a61a91de455d35080bdb2b02061d993b3e417e2
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-29 16:48:46 -0500

    created index js for modules dir

--- models/workout.js
+++ models/workout.js
@@ -11,32 +11,39 @@ const workoutSchema = new Schema({
         {
             type: {
                 type: String,
-                trim: true,
-                required: "Enter a type of exercise"
+                // trim: true,
+                // required: "Enter a type of exercise"
             },
             name: {
                 type: String,
-                trim: true,
-                required: "Enter a name for the exercise"
+                // trim: true,
+                // required: "Enter a name for the exercise"
+            },
+            distance: {
+                type: Number,
+                // required: "Enter the distance"
             },
             duration: {
                 type: Number,
-                required: "Enter the duration"
+                // required: "Enter the duration"
             },
             weight: {
                 type: Number,
-                required: "Enter the weight"
+                // required: "Enter the weight"
             },
             reps: {
                 type: Number,
-                required: "Enter the amount of reps"
+                // required: "Enter the amount of reps"
             },
             sets: {
                 type: Number,
-                required: "Enter the amount of sets"
+                // required: "Enter the amount of sets"
             }
         }
-    ]
+    ],
+    // totalDuration: {
+    //     type: Number,
+    // }
 })
 
 const Workout = mongoose.model("Workout", workoutSchema);
