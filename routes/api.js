d5abc28cd2b48b35bbcad94a6fbcdd621cc173a2
Brianna Hebeler <briannahebeler@gmail.com>
2021-01-02 16:46:04 -0500

    added routwr for get workout data

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,18 @@ const router = require("express").Router();
 const Workout= require("../models/workout.js");
 
 // get workout data //
+router.get("api/workouts", (req, res) => {
+    console.log("inside get workout data");
+    Workout.find({}, (err,data) => {
+        console.log(data);
+        if (err) {
+            res.json(err);
+        }
+        else {
+            res.json(data);
+        }
+    })
+})
 
 // create a new workout session //
 
