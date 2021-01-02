138a61cca758a8c27f0d25e6853ed1718724308a
Brianna Hebeler <briannahebeler@gmail.com>
2021-01-02 17:07:43 -0500

    created router for updating exisiting workout

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-const { Workout } = require("../models/workout.js");
+const Workout = require("../models/workout.js");
 
 // get workout data //
-router.get("api/workouts", (req, res) => {
+router.get("/api/workouts", (req, res) => {
     console.log("inside get workout data");
     Workout.find({})
     .sort({ date: -1 })
@@ -16,7 +16,7 @@ router.get("api/workouts", (req, res) => {
 });
 
 // create a new workout session //
-router.post("api/workouts", ({ body }, res) => {
+router.post("/api/workouts", ({ body }, res) => {
     console.log("inside creation of new workout session");
     console.log(body);
     Workout.create(body)
@@ -30,6 +30,21 @@ router.post("api/workouts", ({ body }, res) => {
 });
 
 // update existing workout //
+router.put("/api/workouts/:id", (req,res) => {
+    console.log("inside of the update workout");
+    Workout.findByIdAndUpdate(
+        req.params.id,
+        { $push: { exercises: req.body } },
+        { new: true }
+    )
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    })
+});
 
 // get stats for all workouts chart //
 
