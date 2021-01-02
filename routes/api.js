5a9f9575f26321b247b5d8a0a26aa697bc818678
Brianna Hebeler <briannahebeler@gmail.com>
2021-01-02 17:00:19 -0500

    redid the get workout functuon, created post fucntion

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,33 @@
 const router = require("express").Router();
-const Workout= require("../models/workout.js");
+const { Workout } = require("../models/workout.js");
 
 // get workout data //
 router.get("api/workouts", (req, res) => {
     console.log("inside get workout data");
-    Workout.find({}, (err,data) => {
-        console.log(data);
-        if (err) {
-            res.json(err);
-        }
-        else {
-            res.json(data);
-        }
+    Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+        res.json(dbWorkout);
     })
-})
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    })
+});
 
 // create a new workout session //
+router.post("api/workouts", ({ body }, res) => {
+    console.log("inside creation of new workout session");
+    console.log(body);
+    Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err)
+    })
+});
 
 // update existing workout //
 
